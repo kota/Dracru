dcf12130fd2f333c306c9fcf598bfc213a872336
curi1119 <curi@CuriMacBook.local>
2010-07-10 00:20:18 +0900

    削除

--- the-target/app/models/time_table.rb
+++
@@ -1,2 +0,0 @@
-class TimeTable < ActiveRecord::Base
-end
--- the-target/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '51de8519d7d7ed165dc0980a1a1de70e5f06d022632878b356032a481ccb140e420b5d5ed105df080cff655b48ab944c6f8191c2bb26f0781022d32e02dfcab5';
